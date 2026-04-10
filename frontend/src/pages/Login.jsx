099348bff455840813c7d816eef0aa1ba4b5ea96
karthikeyan <karthikeyanu2205@gmail.com>
2026-04-10 11:08:06 +0530

    updated the login.jsx line 20

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -16,8 +16,7 @@ function Login() {
             ...formData,
             [e.target.name]:e.target.value
         })        
-    }  
-    console.log(formData)  
+    }
     return (
         <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
             <Card sx={{ 
